0d96d90c4dd8238dd33c2b18200d76e85d68fb47
Victor Wade <p2525657888@gmail.com>
2024-10-01 10:07:07 -1000

    i don't get it

--- src/features/puppies/PuppyDetails.jsx
+++ src/features/puppies/PuppyDetails.jsx
@@ -1,50 +1,55 @@
+import { useGetPuppyQuery, useDeletePuppyMutation } from "./puppySlice";
+
 /**
  * @component
  * Shows comprehensive information about the selected puppy, if there is one.
  * Also provides a button for users to remove the selected puppy from the roster.
  */
 export default function PuppyDetails({ selectedPuppyId, setSelectedPuppyId }) {
-  // TODO: Grab data from the `getPuppy` query
+	// TODO: Grab data from the `getPuppy` query
+	const { data: puppy, isLoading } = useGetPuppyQuery(selectedPuppyId);
 
-  // TODO: Use the `deletePuppy` mutation to remove a puppy when the button is clicked
+	// TODO: Use the `deletePuppy` mutation to remove a puppy when the button is clicked
+	const [deletePuppy] = useDeletePuppyMutation();
 
-  function removePuppy(id) {
-    setSelectedPuppyId();
-  }
+	function removePuppy(id) {
+		setSelectedPuppyId();
+		deletePuppy(id);
+	}
 
-  // There are 3 possibilities:
-  let $details;
-  // 1. A puppy has not yet been selected.
-  if (!selectedPuppyId) {
-    $details = <p>Please select a puppy to see more details.</p>;
-  }
-  //  2. A puppy has been selected, but results have not yet returned from the API.
-  else if (isLoading) {
-    $details = <p>Loading puppy information...</p>;
-  }
-  // 3. Information about the selected puppy has returned from the API.
-  else {
-    $details = (
-      <>
-        <h3>
-          {puppy.name} #{puppy.id}
-        </h3>
-        <p>{puppy.breed}</p>
-        <p>Team {puppy.team?.name ?? "Unassigned"}</p>
-        <button onClick={() => removePuppy(puppy.id)}>
-          Remove from roster
-        </button>
-        <figure>
-          <img src={puppy.imageUrl} alt={puppy.name} />
-        </figure>
-      </>
-    );
-  }
+	// There are 3 possibilities:
+	let $details;
+	// 1. A puppy has not yet been selected.
+	if (!selectedPuppyId) {
+		$details = <p>Please select a puppy to see more details.</p>;
+	}
+	//  2. A puppy has been selected, but results have not yet returned from the API.
+	else if (isLoading) {
+		$details = <p>Loading puppy information...</p>;
+	}
+	// 3. Information about the selected puppy has returned from the API.
+	else {
+		$details = (
+			<>
+				<h3>
+					{puppy.name} #{puppy.id}
+				</h3>
+				<p>{puppy.breed}</p>
+				<p>Team {puppy.team?.name ?? "Unassigned"}</p>
+				<button onClick={() => removePuppy(puppy.id)}>
+					Remove from roster
+				</button>
+				<figure>
+					<img src={puppy.imageUrl} alt={puppy.name} />
+				</figure>
+			</>
+		);
+	}
 
-  return (
-    <aside>
-      <h2>Selected Puppy</h2>
-      {$details}
-    </aside>
-  );
+	return (
+		<aside>
+			<h2>Selected Puppy</h2>
+			{$details}
+		</aside>
+	);
 }
