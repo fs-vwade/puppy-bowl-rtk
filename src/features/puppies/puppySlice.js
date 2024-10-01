8373e7299bf84b068e1961d70f83a6dbbcd9b2bf
Victor Wade <p2525657888@gmail.com>
2024-10-01 08:47:04 -1000

    RTFM

--- src/features/puppies/puppySlice.js
+++ src/features/puppies/puppySlice.js
@@ -17,26 +17,34 @@ functions for each endpoint.
 const puppyApi = api.injectEndpoints({
 	endpoints: (build) => ({
 		getPuppies: build.query({
-			query: () => "puppies",
+			query: () => "players",
+			transformResponse: (response) => response.data.players,
+			transformErrorResponse: (response) => response.data.error,
 			providesTags: ["Puppy"],
 		}),
 		getPuppy: build.query({
-			query: (id) => `puppies/${id}`,
+			query: (id) => `players/${id}`,
 			providesTags: ["Puppy"],
 		}),
+		transformResponse: (response) => response.data.player,
+		transformErrorResponse: (response) => response.data.error,
 		addPuppy: build.mutation({
 			query: (puppy) => ({
-				url: "puppies",
+				url: "players",
 				method: "POST",
 				body: puppy,
 			}),
+			transformResponse: (response) => response.data.newPlayer,
+			transformErrorResponse: (response) => response.data.error,
 			invalidatesTags: ["Puppy"],
 		}),
 		deletePuppy: build.mutation({
 			query: (id) => ({
-				url: `puppies/${id}`,
+				url: `players/${id}`,
 				method: "DELETE",
 			}),
+			transformResponse: (response) => response.success,
+			transformErrorResponse: (response) => response.data.error,
 			invalidatesTags: ["Puppy"],
 		}),
 	}),
